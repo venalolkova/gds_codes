2ac5c405e31d7c85949e8d0199c7792377e5c239
venalolkova <venalolkova@gmail.com>
2024-01-22 14:42:08 +0100

    testing structures example

--- func.py
+++ func.py
@@ -3,6 +3,8 @@ from math import pi
 from gdshelpers.geometry.chip import Cell
 from gdshelpers.parts.waveguide import Waveguide
 from gdshelpers.parts.coupler import GratingCoupler
+from gdshelpers.parts import Port
+from gdshelpers.parts.splitter import MMI
 from gdshelpers.parts.resonator import RingResonator
 from gdshelpers.parts.interferometer import MachZehnderInterferometerMMI
 from gdshelpers.layout import GridLayout
@@ -29,7 +31,7 @@ class CrossingConverterTesting:
         :return: list of geometrical objects with test structure of in/out losses
         '''
         if name is None:
-            na = 'my cell'
+            name = 'inout'
         length1 = length
         length2 = distance - 2 * bending_radius
         coupler_left = GratingCoupler.make_traditional_coupler(origin=origin, **self.coupler_params)
@@ -41,7 +43,7 @@ class CrossingConverterTesting:
         wg.add_straight_segment(length1)
         coupler_right = GratingCoupler.make_traditional_coupler_at_port(wg.port, **self.coupler_params)
 
-        cell = Cell('my cell')
+        cell = Cell(name)
         cell.add_to_layer(1, coupler_left, wg, coupler_right)
 
         return cell
@@ -59,7 +61,7 @@ class CrossingConverterTesting:
         if distance is None:
             distance = 4*bending_radius
         if name is None:
-            na = 'my cell'
+            name = 'bends_array'
         l = distance - 4 * bending_radius
         coupler_left = GratingCoupler.make_traditional_coupler(origin=origin, **self.coupler_params)
         wg = Waveguide.make_at_port(coupler_left.port)
@@ -85,5 +87,149 @@ class CrossingConverterTesting:
 
         cell = Cell(name)
         cell.add_to_layer(1, coupler_left, wg, coupler_right)
+
+        return cell
+    def delay_line (self, length, bending_radius, grating_distance, name=None):
+        dif = 2
+        if name is None:
+            name = 'delay line'
+        d = grating_distance
+        L = length
+        R = bending_radius
+        L_b = pi/2*R
+        # coupler_left = GratingCoupler.make_traditional_coupler(origin=origin, **self.coupler_params)
+
+        wg1 = Waveguide.make_at_port(Port([0, 0], 0, 0.5))
+        wg2 = Waveguide.make_at_port(Port([0, 0], pi, 0.5))
+        wg1.add_bend(pi, R)
+        wg1.add_bend(-pi, R)
+
+        L-=2*pi*R
+        wg2.add_straight_segment(dif)
+        wg2.add_bend(-pi / 2, R)
+        wg2.add_straight_segment(2 * R + dif)
+        wg2.add_bend(-pi / 2, R)
+        L-=(pi+2)*R-2*dif
+        i = 1
+        while L>=0:
+            dif_2 = dif+2*i*dif
+            dif_1 = dif + 2 * (i-1) * dif
+            wg2.add_straight_segment(dif_2)
+            wg2.add_bend(-pi/2, R)
+            wg2.add_straight_segment(2*R+dif_2)
+            wg2.add_bend(-pi/2, R)
+
+            wg1.add_straight_segment(dif_1)
+            wg1.add_bend(-pi/2, R)
+            wg1.add_straight_segment(2*R+dif_1)
+            wg1.add_bend(-pi/2, R)
+
+            L-=2*dif_2+2*dif_1+pi*2*R+4*R
+            i+=1
+
+        wg1.add_straight_segment(d-dif)
+        wg1.add_bend(pi/2, R)
+        wg2.add_bend(pi / 2, R)
+
+        coupler_left = GratingCoupler.make_traditional_coupler_at_port(wg1.port, **self.coupler_params)
+        coupler_right = GratingCoupler.make_traditional_coupler_at_port(wg2.port, **self.coupler_params)
+        cell = Cell(name)
+        cell.add_to_layer(1, coupler_left, wg1, wg2, coupler_right)
+
+        return cell
+
+    def amzi(self, difference, name = None):
+        if name is None:
+            name = 'amzi'
+        coupler_left = GratingCoupler.make_traditional_coupler(
+            origin=[0, 0],
+            **self.coupler_params
+        )
+        wg1 = Waveguide.make_at_port(coupler_left.port)
+        wg1.add_straight_segment(10)
+        wg1.add_bend(-pi/2, 20)
+        wg1.add_straight_segment(10)
+
+        mmi1 = MMI.make_at_port(wg1.port, 16, 4, 1, 2)
+        wg1_1 = Waveguide.make_at_port(mmi1.output_ports[0])
+        wg1_1.add_bend(-pi/2, 20)
+        wg1_1.add_bend(pi/2, 20)
+        wg1_1.add_straight_segment(100)
+        wg1_1.add_bend(pi/2, 20)
+        wg1_1.add_bend(-pi/2, 20)
+
+        wg1_2 = Waveguide.make_at_port(mmi1.output_ports[1])
+        wg1_2.add_bend(pi/2, 20)
+        wg1_2.add_straight_segment(difference/2)
+        wg1_2.add_bend(-pi/2, 20)
+        wg1_2.add_straight_segment(100)
+        wg1_2.add_bend(-pi/2, 20)
+        wg1_2.add_straight_segment(difference/2)
+        wg1_2.add_bend(pi/2, 20)
+
+        mmi2 = MMI.make_at_port(wg1_2.port, 16, 4, 2, 1)
+
+        wg2 = Waveguide.make_at_port(mmi2.output_ports[0])
+        wg2.add_straight_segment(10)
+        wg2.add_bend(-pi/2, 20)
+        wg2.add_straight_segment(10)
+
+        coupler_right = GratingCoupler.make_traditional_coupler_at_port(
+            wg2.port,
+            **self.coupler_params
+        )
+
+
+        cell = Cell(name)
+        cell.add_to_layer(1, coupler_left, wg1, mmi1, wg1_1, wg1_2, mmi2, wg2, coupler_right)
+
+        return cell
+
+    def amzi_converter(self, difference, name = None):
+        if name is None:
+            name = 'amzi'
+        coupler_left = GratingCoupler.make_traditional_coupler(
+            origin=[0, 0],
+            **self.coupler_params
+        )
+        wg1 = Waveguide.make_at_port(coupler_left.port)
+        wg1.add_straight_segment(10)
+        wg1.add_bend(-pi/2, 20)
+        wg1.add_straight_segment(10)
+
+        mmi1 = MMI.make_at_port(wg1.port, 16, 4, 1, 2)
+        wg1_1 = Waveguide.make_at_port(mmi1.output_ports[0])
+        wg1_1.add_bend(-pi/2, 20)
+        wg1_1.add_bend(pi/2, 20)
+        wg1_1.add_straight_segment(10, 0.5)
+        wg1_1_1 = Waveguide.make_at_port(port=Port([wg1_1.port.origin[0]+80, wg1_1.port.origin[1]], 0, 0.5))
+        wg1_1_1.add_straight_segment(10, 1.3)
+        wg1_1_1.add_bend(pi/2, 20)
+        wg1_1_1.add_bend(-pi/2, 20)
+
+        wg1_2 = Waveguide.make_at_port(mmi1.output_ports[1])
+        wg1_2.add_bend(pi/2, 20)
+        wg1_2.add_straight_segment(difference/2)
+        wg1_2.add_bend(-pi/2, 20)
+        wg1_2.add_straight_segment(100)
+        wg1_2.add_bend(-pi/2, 20)
+        wg1_2.add_straight_segment(difference/2)
+        wg1_2.add_bend(pi/2, 20)
+
+        mmi2 = MMI.make_at_port(wg1_2.port, 16, 4, 2, 1)
+
+        wg2 = Waveguide.make_at_port(mmi2.output_ports[0])
+        wg2.add_straight_segment(10)
+        wg2.add_bend(-pi/2, 20)
+        wg2.add_straight_segment(10)
+
+        coupler_right = GratingCoupler.make_traditional_coupler_at_port(
+            wg2.port,
+            **self.coupler_params
+        )
+
+
+        cell = Cell(name)
+        cell.add_to_layer(1, coupler_left, wg1, mmi1, wg1_1, wg1_1_1, wg1_2, mmi2, wg2, coupler_right)
 
         return cell
--- test.py
+++ test.py
@@ -8,25 +8,58 @@ from func import *
 from gdshelpers.helpers import id_to_alphanumeric
 
 structure = CrossingConverterTesting()
-io = structure.inout(10, 150, 10, name = '1')
-ba = structure.bends_array(10, 52, name = '2')
+io = structure.inout(10, 150, 10, name='in_out')
+ba1 = structure.bends_array(10, 52, name='52 bends')
+ba2 = structure.bends_array(10, 100, name='100 bends')
+dl1 = structure.delay_line(10000, 10, 100, name='10 cm delay line')
+dl2 = structure.delay_line(15000, 10, 100, name='15 cm delay line')
+amzi = structure.amzi(10, 'amzi1')
+amzi_chiplet = structure.amzi_converter(10, 'amzi+chiplet')
 
 cell_main = Cell("testchip_big")
 
 
-cell_id = id_to_alphanumeric(0, 1)
+cell_id = id_to_alphanumeric(0, 0)
 cell = Cell(cell_id)
 cell.add_cell(io)
-cell.add_to_layer(1, Text([-30, 0], 20, f'{cell_id}'))
+cell.add_to_layer(1, Text([-30, -70], 20, f'{cell_id}'))
+cell_main.add_cell(cell, origin=(0, 0))
+
+cell_id = id_to_alphanumeric(1, 0)
+cell = Cell(cell_id)
+cell.add_cell(ba1)
+cell.add_to_layer(1, Text([-30, -70], 20, f'{cell_id}'))
 cell_main.add_cell(cell, origin=(200, 0))
 
-
-cell_id = id_to_alphanumeric(1, 1)
+cell_id = id_to_alphanumeric(2, 0)
 cell = Cell(cell_id)
-cell.add_cell(ba)
-cell.add_to_layer(1, Text([-30, 0], 20, f'{cell_id}'))
-cell_main.add_cell(cell, origin=(400, 0))
+cell.add_cell(ba2)
+cell.add_to_layer(1, Text([-30, -70], 20, f'{cell_id}'))
+cell_main.add_cell(cell, origin=(350, 0))
+
+cell_id = id_to_alphanumeric(3, 0)
+cell = Cell(cell_id)
+cell.add_cell(dl1)
+cell.add_to_layer(1, Text([-100, -100], 20, f'{cell_id}'))
+cell_main.add_cell(cell, origin=(550, 0))
+
+cell_id = id_to_alphanumeric(4, 0)
+cell = Cell(cell_id)
+cell.add_cell(dl2)
+cell.add_to_layer(1, Text([-100, -100], 20, f'{cell_id}'))
+cell_main.add_cell(cell, origin=(750, 0))
+
+cell_id = id_to_alphanumeric(0, 1)
+cell = Cell(cell_id)
+cell.add_cell(amzi)
+cell.add_to_layer(1, Text([-100, -100], 20, f'{cell_id}'))
+cell_main.add_cell(cell, origin=(0, -200))
+
+cell_id = id_to_alphanumeric(0, 2)
+cell = Cell(cell_id)
+cell.add_cell(amzi_chiplet)
+cell.add_to_layer(1, Text([-100, -100], 20, f'{cell_id}'))
+cell_main.add_cell(cell, origin=(400, -200))
 
 cell_main.save()
-
 
